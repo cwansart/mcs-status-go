6a470cc0be25db0e58009c157d66baf976ad8e77
Christian Wansart <cwansart@users.noreply.github.com>
2023-01-21 21:51:34 +0100

    refactor to read config just once

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 )
 
 func main() {
+	url := settings.Get(settings.ServerUrlKey)
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		url := settings.Get(settings.ServerUrlKey)
 		s := status.Get(url)
 		c.JSON(http.StatusOK, s)
 	})
