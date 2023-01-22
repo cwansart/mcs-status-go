59cec6c60f0b31a3751e7de349a7651416904c99
Christian Wansart <cwansart@users.noreply.github.com>
2023-01-22 10:46:55 +0100

    add test for json unmarshalling error

--- status/status_test.go
+++ status/status_test.go
@@ -56,3 +56,19 @@ func TestInvalidJsonServerResponse(t *testing.T) {
 		t.Errorf("PlayerCount should be 0")
 	}
 }
+
+func TestJsonUnmarshallingError(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`[ini]\nconf=test`))
+	}))
+	defer s.Close()
+
+	r := status.Get(s.URL)
+
+	if r.IsOnline == false {
+		t.Errorf("IsOnline should be true")
+	}
+	if r.PlayerCount > 0 {
+		t.Errorf("PlayerCount should be 0")
+	}
+}
