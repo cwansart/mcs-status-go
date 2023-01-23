3d2a0e340f9766286fb2592c2226beb1f1b09ab7
Christian Wansart <cwansart@users.noreply.github.com>
2023-01-23 19:01:38 +0100

    extract functionality into functions in settings

--- settings/settings.go
+++ settings/settings.go
@@ -3,15 +3,11 @@ package settings
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"os"
 )
 
-const (
-	configFile = "config.json"
-	configPath = "."
-)
+const configFile = "./config.json"
 
 type Config struct {
 	ServerUrl string `json:"serverurl"`
@@ -21,25 +17,32 @@ var config = Config{
 	ServerUrl: "http://localhost:2006",
 }
 
+func createConfigFile(p string) {
+	log.Printf("Could not find config file, generating one using defaults in %v", p)
+	b, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		log.Fatal("Could not marshal config data to json")
+	}
+	os.WriteFile(p, b, 0600)
+}
+
+func loadConfigFile(p string) {
+	b, err := os.ReadFile(p)
+	if err != nil {
+		log.Fatalf("Could not open config file %v", p)
+	}
+	json.Unmarshal(b, &config)
+}
+
 func ReadConfigFile() Config {
 	log.Println("Loading config file")
-	p := fmt.Sprintf("%v/%v", configPath, configFile)
 
-	if _, err := os.Stat(p); errors.Is(err, os.ErrNotExist) {
-		log.Printf("Could not find config file, generating one using defaults in %v", p)
-		b, err := json.MarshalIndent(config, "", "  ")
-		if err != nil {
-			log.Fatal("Could not marshal config data to json")
-		}
-		os.WriteFile(p, b, 0600)
+	if _, err := os.Stat(configFile); errors.Is(err, os.ErrNotExist) {
+		createConfigFile(configFile)
 	} else {
-		b, err := os.ReadFile(p)
-		if err != nil {
-			log.Fatalf("Could not open config file %v", p)
-		}
-		json.Unmarshal(b, &config)
+		loadConfigFile(configFile)
 	}
-	log.Println("Successfully read config file", p, config.ServerUrl)
+	log.Println("Successfully read config file", configFile, config.ServerUrl)
 
 	return config
 }
