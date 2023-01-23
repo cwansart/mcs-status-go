5987dd7520fd608966f5b82063a8e04137165d29
Christian Wansart <cwansart@users.noreply.github.com>
2023-01-23 12:47:49 +0100

    replace gin with net/http

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"de.cwansart.mcss/settings"
 	"de.cwansart.mcss/status"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	url := settings.Get(settings.ServerUrlKey)
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		s := status.Get(url)
-		c.JSON(http.StatusOK, s)
+		b, err := json.Marshal(s)
+
+		if err != nil {
+			log.Println("Failed to parse json:", s)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 internal server error"))
+		} else {
+			log.Println("Receive request, response:", string(b))
+			w.Header().Add("Content-Type", "application/json")
+			w.Write(b)
+		}
 	})
-	r.Run()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
