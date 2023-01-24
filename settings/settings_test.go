b1bd094080b14225d5d0d3301f119e2b5d071439
Christian Wansart <cwansart@users.noreply.github.com>
2023-01-24 21:11:35 +0100

    add settings_test

---
+++ settings/settings_test.go
@@ -0,0 +1,67 @@
+package settings
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestSettings(t *testing.T) {
+	t.Run("create new config file", func(t *testing.T) {
+		t.Parallel()
+		configFile := getTempConfigFileName(t)
+
+		NewConfig(configFile)
+
+		if _, err := os.Stat(configFile); errors.Is(err, os.ErrNotExist) {
+			t.Errorf("Test should have created config file '%s' but did not", configFile)
+		}
+	})
+
+	t.Run("should use default config", func(t *testing.T) {
+		t.Parallel()
+		configFile := getTempConfigFileName(t)
+
+		got := NewConfig(configFile)
+
+		want := "http://localhost:2006"
+		if got.ServerUrl != want {
+			t.Errorf("ServerUrl should be %s", want)
+		}
+	})
+
+	t.Run("read stored config file", func(t *testing.T) {
+		configFile := getTempConfigFileName(t)
+		saveAsFile(t, Config{"http://localhost:8888"}, configFile)
+
+		got := NewConfig(configFile)
+
+		want := "http://localhost:8888"
+		if got.ServerUrl != want {
+			t.Errorf("ServerUrl should be %s", want)
+		}
+	})
+}
+
+func getTempConfigFileName(t *testing.T) (c string) {
+	t.Helper()
+	dir := t.TempDir()
+	c = fmt.Sprintf("%s/%s", dir, "config.json")
+	return c
+}
+
+func saveAsFile(t *testing.T, c Config, f string) {
+	t.Helper()
+
+	json, err := json.Marshal(c)
+	if err != nil {
+		t.Error("Failed to create json test data")
+	}
+
+	err = os.WriteFile(f, []byte(json), 0600)
+	if err != nil {
+		t.Error("Failed to write temp config file")
+	}
+}
