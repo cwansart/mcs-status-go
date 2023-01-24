edbb0fa1ce336bcd5465a28637a3d7468080a32a
Christian Wansart <cwansart@users.noreply.github.com>
2023-01-24 21:17:06 +0100

    put status tests in sub tests

--- status/status_test.go
+++ status/status_test.go
@@ -6,75 +6,76 @@ import (
 	"testing"
 )
 
-func TestIsOnline(t *testing.T) {
-	t.Parallel()
+func TestStatus(t *testing.T) {
+	t.Run("test if is online", func(t *testing.T) {
+		t.Parallel()
 
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"players": {"count": 1}}`))
-	}))
-	defer s.Close()
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`{"players": {"count": 1}}`))
+		}))
+		defer s.Close()
 
-	r := Get(s.URL)
+		r := Get(s.URL)
 
-	if r.IsOnline == false {
-		t.Errorf("IsOnline should be true")
-	}
-	if r.PlayerCount != 1 {
-		t.Errorf("PlayerCount should be 1")
-	}
-}
+		if r.IsOnline == false {
+			t.Errorf("IsOnline should be true")
+		}
+		if r.PlayerCount != 1 {
+			t.Errorf("PlayerCount should be 1")
+		}
+	})
 
-func TestIsOffline(t *testing.T) {
-	t.Parallel()
+	t.Run("test if is offline", func(t *testing.T) {
+		t.Parallel()
 
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		}))
+		url := s.URL
+		s.Close()
 
-	}))
-	url := s.URL
-	s.Close()
+		r := Get(url)
 
-	r := Get(url)
+		if r.IsOnline == true {
+			t.Errorf("IsOnline should be false")
+		}
+		if r.PlayerCount > 0 {
+			t.Errorf("PlayerCount should be 0")
+		}
+	})
 
-	if r.IsOnline == true {
-		t.Errorf("IsOnline should be false")
-	}
-	if r.PlayerCount > 0 {
-		t.Errorf("PlayerCount should be 0")
-	}
-}
+	t.Run("test invalid json response from server", func(t *testing.T) {
+		t.Parallel()
 
-func TestInvalidJsonServerResponse(t *testing.T) {
-	t.Parallel()
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`{}`))
+		}))
+		defer s.Close()
 
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{}`))
-	}))
-	defer s.Close()
+		r := Get(s.URL)
 
-	r := Get(s.URL)
+		if r.IsOnline == false {
+			t.Errorf("IsOnline should be true")
+		}
+		if r.PlayerCount > 0 {
+			t.Errorf("PlayerCount should be 0")
+		}
+	})
 
-	if r.IsOnline == false {
-		t.Errorf("IsOnline should be true")
-	}
-	if r.PlayerCount > 0 {
-		t.Errorf("PlayerCount should be 0")
-	}
-}
+	t.Run("test json unmarshalling error", func(t *testing.T) {
+		t.Parallel()
 
-func TestJsonUnmarshallingError(t *testing.T) {
-	t.Parallel()
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`[ini]\nconf=test`))
+		}))
+		defer s.Close()
 
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`[ini]\nconf=test`))
-	}))
-	defer s.Close()
+		r := Get(s.URL)
 
-	r := Get(s.URL)
-
-	if r.IsOnline == true {
-		t.Errorf("IsOnline should be false")
-	}
-	if r.PlayerCount > 0 {
-		t.Errorf("PlayerCount should be 0")
-	}
+		if r.IsOnline == true {
+			t.Errorf("IsOnline should be false")
+		}
+		if r.PlayerCount > 0 {
+			t.Errorf("PlayerCount should be 0")
+		}
+	})
 }
